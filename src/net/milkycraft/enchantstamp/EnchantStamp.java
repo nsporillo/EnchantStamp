ae895e825b62b4b24dd3c6f8a1f7bb5337618694
Nick Porillo <milkywayz@mail.com>
2014-05-05 19:30:16 -0400

    Initial Commit

---
+++ src/net/milkycraft/enchantstamp/EnchantStamp.java
@@ -0,0 +1,87 @@
+package net.milkycraft.enchantstamp;
+
+import static org.bukkit.ChatColor.GOLD;
+import static org.bukkit.ChatColor.GREEN;
+import static org.bukkit.ChatColor.RED;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnchantStamp extends JavaPlugin implements Listener {
+
+	private String gmperm;
+	private SimpleDateFormat sdf;
+
+	@Override
+	public void onEnable() {
+		this.saveDefaultConfig();
+		this.sdf = new SimpleDateFormat(this.getConfig().getString("SimpleDateFormat"));
+		this.gmperm = this.getConfig().getString("Creative_Permission");
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onEnchant(EnchantItemEvent e) {
+		Player p = e.getEnchanter();
+		ItemStack is = e.getItem();
+		this.generateLore(p, is, "Enchanted", "Enchanted");
+	}
+
+	@SuppressWarnings("deprecation")
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onAnvil(InventoryClickEvent e) {
+		Inventory i = e.getInventory();
+		if (!(e.getWhoClicked() instanceof Player)) {
+			return;
+		}
+		if (e.getSlotType() != SlotType.RESULT) {
+			return;
+		}
+		ItemStack item = e.getCurrentItem();
+		if (item.getTypeId() == 0) {
+			return;
+		}
+		if (i.getType() == InventoryType.ANVIL) {
+			Player p = (Player) e.getWhoClicked();
+			this.generateLore(p, item, "Modified", "Modified");
+		} else if (i.getType() == InventoryType.MERCHANT) {
+			if (item.getEnchantments().size() > 0) {
+				Player p = (Player) e.getWhoClicked();
+				this.generateLore(p, item, "Traded with Villager", "Obtained");
+			}
+		}
+	}
+
+	
+	private void generateLore(Player p, ItemStack item, String pre, String suff) {
+		ItemMeta im = item.getItemMeta();
+		List<String> s = new ArrayList<String>();
+		if (p.getGameMode() == GameMode.CREATIVE) {
+			s.add(RED + pre + " by " + GOLD + p.getName() + RED
+					+ " in creative");
+		} else if (p.hasPermission(this.gmperm)) {
+			s.add(RED + pre + " by " + GOLD + p.getName() + RED
+					+ " (Potentially in creative)");
+		} else {
+			s.add(GREEN + pre + " by " + p.getName());
+		}
+		s.add(GOLD + suff + " on " + sdf.format(new Date()));
+		im.setLore(s);
+		item.setItemMeta(im);
+	}
+}
